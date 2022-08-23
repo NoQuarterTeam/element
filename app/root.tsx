9fdee341787b84fac872158c2b08ec473b2d6221
Jack Clackett <jack@noquarter.co>
2022-08-23 21:24:33 +0200

    fix: change root color

--- app/root.tsx
+++ app/root.tsx
@@ -137,9 +137,9 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache) =>
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#000" />
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#000" />
         <Meta />
         <Links />
         {serverSyleData?.map(({ key, ids, css }) => (
