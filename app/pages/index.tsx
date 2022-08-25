22ff9dc8ebbff19f7e4df8d711c4991d9b3f9d9b
Jack Clackett <jack@noquarter.co>
2022-08-25 13:17:15 +0200

    feat: temp home page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,9 @@
 import * as c from "@chakra-ui/react"
-import { Link } from "@remix-run/react"
-import type { LoaderArgs, MetaFunction} from "@remix-run/server-runtime";
+import type { LoaderArgs, MetaFunction } from "@remix-run/server-runtime"
 import { redirect } from "@remix-run/server-runtime"
 import { json } from "@remix-run/server-runtime"
 
+import { LinkButton } from "~/components/LinkButton"
 import { getUser } from "~/services/auth/auth.server"
 
 export const meta: MetaFunction = () => {
@@ -25,10 +25,21 @@ export const loader = async ({ request }: LoaderArgs) => {
 export default function HomeLayout() {
   return (
     <c.LightMode>
-      <c.Center pt={8}>
-        <c.VStack>
+      <c.Center pt={8} h="100vh" w="100vw" maxH="-webkit-fill-available">
+        <c.VStack spacing={8} textAlign="center">
           <c.Image src="/logo.png" boxSize="100px" />
-          <Link to="/login">Login</Link>
+          <c.VStack>
+            <c.Heading>Your new life planner</c.Heading>
+            <c.Text>We are currently in beta, more details coming soon!</c.Text>
+          </c.VStack>
+          <c.HStack>
+            <LinkButton w="100px" size="md" colorScheme="orange" to="/register">
+              Join now
+            </LinkButton>
+            <LinkButton size="md" variant="ghost" w="100px" to="/login">
+              Login
+            </LinkButton>
+          </c.HStack>
         </c.VStack>
       </c.Center>
     </c.LightMode>
