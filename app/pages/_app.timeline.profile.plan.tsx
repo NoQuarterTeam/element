dd3b72e3daa8dd8532f4531e6022265c5646b563
Jack Clackett <jack@noquarter.co>
2023-01-19 23:52:45 +0100

    fix: lock file

--- app/pages/_app.timeline.profile.plan.tsx
+++ app/pages/_app.timeline.profile.plan.tsx
@@ -1,6 +1,5 @@
 import type { ActionArgs, LoaderArgs, SerializeFrom } from "@remix-run/node"
-import { json } from "@remix-run/node"
-import { redirect } from "@remix-run/node"
+import { json, redirect } from "@remix-run/node"
 import { Outlet, useFetcher, useLoaderData } from "@remix-run/react"
 import dayjs from "dayjs"
 
