6c5322d646c225bc76e402d4a8448b7e383aeb83
Jack Clackett <jack@noquarter.co>
2023-01-26 21:56:28 +0100

    fix: admin

--- app/pages/admin.tsx
+++ app/pages/admin.tsx
@@ -11,6 +11,7 @@ import { Badge } from "~/components/ui/Badge"
 import { LinkButton } from "~/components/ui/LinkButton"
 import { db } from "~/lib/db.server"
 import { getUser } from "~/services/auth/auth.server"
+import { Limiter } from "~/components/ui/Limiter"
 
 export const loader = async ({ request }: LoaderArgs) => {
   const user = await getUser(request)
@@ -19,7 +20,13 @@ export const loader = async ({ request }: LoaderArgs) => {
     db.user.findMany({
       where: { role: Role.USER },
       orderBy: { createdAt: "desc" },
-      select: { id: true, firstName: true, email: true, stripeSubscriptionId: true },
+      select: {
+        id: true,
+        firstName: true,
+        email: true,
+        stripeSubscriptionId: true,
+        _count: { select: { tasks: true, elements: true } },
+      },
     }),
     db.task.count(),
     db.task.count({
@@ -49,51 +56,69 @@ export default function Admin() {
   const { users, taskCountTotal, tastCountLastMonth, taskCountThisMonth, feedback } = useLoaderData<typeof loader>()
   const percentageChange = Math.round((taskCountThisMonth / (tastCountLastMonth || 1) - 1) * 100)
   return (
-    <div className="p-6">
-      <div>
-        <LinkButton to="/timeline" variant="ghost" leftIcon={<RiArrowLeftLine />}>
-          Back to timeline
-        </LinkButton>
-      </div>
-      <h2>Admin</h2>
-      <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
-        <div className="space-y-6">
+    <Limiter>
+      <div className="space-y-6 p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex-1">
+            <LinkButton to="/timeline" variant="outline" leftIcon={<RiArrowLeftLine />}>
+              Back to timeline
+            </LinkButton>
+          </div>
+          <h2 className="flex-1 text-center text-xl">Admin</h2>
+          <div className="flex-1" />
+        </div>
+        <div className="flex justify-between border border-gray-100 p-4 dark:border-gray-700">
           <div>
             <h4 className="text-lg">Users</h4>
             <p className="text-3xl">{users.length}</p>
           </div>
+          <div>
+            <h4 className="text-lg">Total tasks</h4>
+            <p className="text-3xl">{taskCountTotal.toLocaleString()}</p>
+          </div>
+          <div className="stack">
+            <h4>Tasks this month</h4>
+            <div className="hstack">
+              <p className="text-3xl">{taskCountThisMonth}</p>
+              <div className="hstack text-sm opacity-50">
+                {percentageChange < 0 ? <IoMdArrowDropdown className="sq-5" /> : <IoMdArrowDropup className="sq-5" />}
+                {Math.abs(percentageChange)}%
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="stack">
+          <div className="grid grid-cols-5 text-xs font-semibold uppercase text-gray-500 dark:text-gray-400">
+            <p>Name</p>
+            <p>Email</p>
+            <p>Tasks</p>
+            <p>Elements</p>
+            <p className="text-right">Subscription</p>
+          </div>
+
           <div className="stack">
             {users.map((user) => (
-              <div className="text-sm" key={user.id}>
+              <div className="grid grid-cols-5 rounded border border-gray-100 p-4 text-sm dark:border-gray-700" key={user.id}>
                 <p>{user.firstName}</p>
                 <p>{user.email}</p>
-                {user.stripeSubscriptionId && <Badge colorScheme="primary">Pro</Badge>}
+                <p>{user._count.tasks}</p>
+                <p>{user._count.elements}</p>
+                <div className="flex justify-end">
+                  {user.stripeSubscriptionId ? <Badge colorScheme="red">Pro</Badge> : <Badge>Free</Badge>}
+                </div>
               </div>
             ))}
           </div>
         </div>
-        <div className="space-y-6">
-          <div>
-            <h4 className="text-lg">Tasks</h4>
-            <p className="text-3xl">{taskCountTotal.toLocaleString()}</p>
-          </div>
-          <div className="stack">
-            <p>This month</p>
-            <p className="text-3xl">{taskCountThisMonth}</p>
-            <div className="hstack">
-              {percentageChange < 0 ? <IoMdArrowDropdown className="sq-8" /> : <IoMdArrowDropup className="sq-8" />}
-              {Math.abs(percentageChange)}%
-            </div>
-          </div>
-        </div>
-        <div className="space-y-6">
+
+        <div className="stack">
           <div>
             <h4 className="text-lg">Feedback</h4>
           </div>
-          <div>
+          <div className="stack">
             {feedback.map((feedback) => (
-              <div className="stack text-sm" key={feedback.id}>
-                <p>{feedback.content}</p>
+              <div className="grid grid-cols-4 border border-gray-100  p-4 text-sm dark:border-gray-700" key={feedback.id}>
+                <p className="col-span-2 truncate">{feedback.content}</p>
                 <p>{feedback.type}</p>
                 <p>{feedback.creator.email}</p>
               </div>
@@ -101,6 +126,6 @@ export default function Admin() {
           </div>
         </div>
       </div>
-    </div>
+    </Limiter>
   )
 }
