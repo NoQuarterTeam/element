f970dfd52206ffb38d0414db30f1ea4c98e9a5d7
Jack Clackett <jack@noquarter.co>
2023-01-27 16:48:38 +0100

    fix: admin filter archived

--- app/pages/admin.tsx
+++ app/pages/admin.tsx
@@ -41,7 +41,7 @@ export const loader = async ({ request }: LoaderArgs) => {
   const [users, userCount, taskCountTotal, tastCountLastMonth, taskCountThisMonth, feedback, usersAgg, activeUsersAgg] =
     await Promise.all([
       db.user.findMany({
-        where: { role: Role.USER },
+        where: { role: Role.USER, archivedAt: { equals: null } },
         orderBy: { createdAt: "desc" },
         take: 5,
         select: {
@@ -52,7 +52,7 @@ export const loader = async ({ request }: LoaderArgs) => {
           _count: { select: { tasks: true, elements: true } },
         },
       }),
-      db.user.count(),
+      db.user.count({ where: { role: Role.USER, archivedAt: { equals: null } } }),
       db.task.count(),
       db.task.count({
         where: {
@@ -84,6 +84,8 @@ export const loader = async ({ request }: LoaderArgs) => {
         SELECT series.date, COUNT("User".id)::int
         FROM series
         LEFT JOIN "User" ON date_trunc(${period}, "User"."createdAt"::date) <= series.date
+          AND "User"."role" = 'USER'
+          AND "User"."archivedAt" IS NULL
         GROUP BY series.date
         ORDER BY series.date ASC
     `,
@@ -97,8 +99,11 @@ export const loader = async ({ request }: LoaderArgs) => {
         )
         SELECT series.date as date, COUNT(distinct "creatorId")::int as count
         FROM series
-        LEFT JOIN "Task" ON date_trunc(${period}, "createdAt") = series.date AND "Task"."isTemplate" = false
+        LEFT JOIN "Task" ON date_trunc(${period}, "createdAt") = series.date
+          AND "Task"."isTemplate" = false
         LEFT JOIN "User" ON "Task"."creatorId" = "User".id
+          AND "User"."role" = 'USER'
+          AND "User"."archivedAt" IS NULL
         GROUP BY series.date
         ORDER BY series.date ASC
     `,
