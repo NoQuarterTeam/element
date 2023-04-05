7293592a4ff01cd7308b86e2fd7eab68da5e76e8
Jack Clackett <jack@noquarter.co>
2023-04-05 21:04:05 +0200

    v1.0.6

--- apps/app/app.config.ts
+++ apps/app/app.config.ts
@@ -6,7 +6,7 @@ const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   slug: "element-app",
   scheme: "elementapp",
   owner: "noquarter",
-  version: "1.0.5",
+  version: "1.0.6",
   jsEngine: "hermes",
   orientation: "portrait",
   icon: "./assets/icon.png",
