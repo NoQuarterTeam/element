9189511a5dfe153c88191916cdcff1a75bfc36f0
Jack Clackett <jack@noquarter.co>
2023-04-05 16:37:18 +0200

    add expo update checks

--- apps/app/src/app/_layout.tsx
+++ apps/app/src/app/_layout.tsx
@@ -1,12 +1,14 @@
+import * as React from "react"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { Slot, SplashScreen } from "expo-router"
 import { StatusBar } from "expo-status-bar"
+import * as Updates from "expo-updates"
 import { ActionSheetProvider } from "@expo/react-native-action-sheet"
+import { View } from "react-native"
 import { useFonts, Poppins_400Regular, Poppins_700Bold, Poppins_900Black, Poppins_600SemiBold } from "@expo-google-fonts/poppins"
 
 import { TRPCProvider } from "../lib/utils/api"
-import { View } from "react-native"
-// import { useColorScheme } from "react-native"
+import { IS_DEV } from "../lib/config"
 
 // This is the main layout of the app
 // It wraps your pages with the providers they need
@@ -17,9 +19,26 @@ export default function RootLayout() {
     Poppins_700Bold,
     Poppins_900Black,
   })
+  const [isFinishedCheckingUpdates, setisFinishedCheckingUpdates] = React.useState(false)
+
+  React.useEffect(() => {
+    if (IS_DEV) return
+    async function expoUpdates() {
+      try {
+        const { isAvailable } = await Updates.checkForUpdateAsync()
+        if (!isAvailable) return setisFinishedCheckingUpdates(true)
+        const { isNew } = await Updates.fetchUpdateAsync()
+        if (!isNew) return setisFinishedCheckingUpdates(true)
+        await Updates.reloadAsync()
+      } catch (e) {
+        // console.log(e);
+      }
+    }
+    expoUpdates()
+  }, [])
 
   // Prevent rendering until the font has loaded
-  if (!fontsLoaded) return <SplashScreen />
+  if (!fontsLoaded || !isFinishedCheckingUpdates) return <SplashScreen />
 
   return (
     <ActionSheetProvider>
