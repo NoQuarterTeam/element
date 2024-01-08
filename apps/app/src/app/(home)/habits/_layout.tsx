d8ba8320a1e9a96713256a2c050d1936de4f63c4
Jack Clackett <jack@noquarter.co>
2024-01-08 10:51:12 +0100

    fix habit index layout

--- apps/app/src/app/(home)/habits/_layout.tsx
+++ apps/app/src/app/(home)/habits/_layout.tsx
@@ -6,7 +6,7 @@ export default function HabitsLayout() {
   const backgroundColor = useBackgroundColor()
   return (
     <Stack screenOptions={{ contentStyle: { backgroundColor }, headerShown: false }}>
-      <Stack.Screen name="index" options={{ presentation: "modal" }} />
+      <Stack.Screen name="index" />
       <Stack.Screen name="new" options={{ presentation: "modal" }} />
       <Stack.Screen name="[id]" options={{ presentation: "modal" }} />
     </Stack>
