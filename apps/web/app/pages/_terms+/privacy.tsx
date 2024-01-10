cfb97c5fc74242c48b1758c9249533c7067c24fd
Jack Clackett <jack@noquarter.co>
2024-01-10 22:23:44 +0100

    add privacy policy

---
+++ apps/web/app/pages/_terms+/privacy.tsx
@@ -0,0 +1,119 @@
+export default function Privacy() {
+  return (
+    <div className="space-y-6 pb-20">
+      <h1 className="text-3xl font-bold">Privacy policy</h1>
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">Introduction:</h2>
+        <p>
+          Thank you for choosing our platform. This Privacy Policy outlines how we collect, use, and protect your personal
+          information when you use our platform. It is important to us that you understand what information we collect and how we
+          use it. By signing up and using our platform, you agree to the terms of this Privacy Policy.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">1. Information we collect:</h2>
+        <p>We collect the following information from you:</p>
+        <ul>
+          <li>
+            Email address: We collect your email address to create and manage your account, and to communicate important updates
+            and notifications to you.
+          </li>
+          <li>Password: To secure your account, we collect a password that you create during the signup process.</li>
+          <li>First Name, and Last Name: These details are collected to personalize your experience on our platform.</li>
+          <li>
+            Tasks and Habits: We collect the tasks and habits you create in order to provide you with the desired features and
+            functionality of our platform.
+          </li>
+          <li>Images: Adding an avatar to your profile.</li>
+        </ul>
+      </div>
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">2. Use of collected information:</h2>
+        <p>We use the collected information for the following purposes:</p>
+        <ul>
+          <li>
+            Providing and improving our platform: Your information allows us to deliver the services and features you expect and
+            enhance your overall experience.
+          </li>
+          <li>
+            Communication: We may use your email address to send you administrative notifications, updates, and relevant
+            information.
+          </li>
+        </ul>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">3. Sharing of information:</h2>
+        <p>We may share your information in the following circumstances:</p>
+        <ul>
+          <li>
+            Aggregated and anonymized data: We may share aggregated and anonymized data that does not personally identify you for
+            research, analysis, or marketing purposes.
+          </li>
+        </ul>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">4. Data security and retention:</h2>
+        <p>
+          We employ industry-standard security measures to protect your personal information from unauthorized access, disclosure,
+          or alteration. However, no method of transmission over the internet or method of electronic storage is 100% secure. We
+          will retain your personal information for as long as necessary to fulfill the purposes outlined in this Privacy Policy,
+          unless a longer retention period is required or permitted by law.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">5. Third-party services:</h2>
+        <p>
+          Our platform may include links to third-party websites or services. Please note that this Privacy Policy does not cover
+          the practices of these third parties. We encourage you to review the privacy policies of those third parties before
+          accessing their websites or using their services.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">6. Children's information:</h2>
+        <p>
+          Our platform is not intended for use by individuals under the age of 13. We do not knowingly collect personal
+          information from children. If you are a parent or legal guardian and believe that your child has provided personal
+          information to us, please contact us immediately.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">7. Your choices:</h2>
+        <p>
+          You have the right to access, correct, update, or delete your personal information. You can manage your account settings
+          within the platform or contact us for assistance.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">8. Changes to this Privacy Policy:</h2>
+        <p>
+          We may update this Privacy Policy from time to time to reflect changes in our practices or legal requirements. We will
+          notify you of any significant changes by posting a prominent notice on our platform or by sending you an email.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">9. Contact us:</h2>
+        <p>
+          If you have any questions, concerns, or requests regarding this Privacy Policy or our privacy practices, please contact
+          us at <a href="mailto:info@noquarter.co">info@noquarter.co</a>.
+        </p>
+      </div>
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">Conclusion:</h2>
+        <p>
+          Protecting your privacy is important to us. By using our platform, you agree to the terms of this Privacy Policy and
+          consent to the collection, use, and sharing of your personal information as described above. We appreciate your trust in
+          us and will always strive to handle your personal information with care and respect.
+        </p>
+      </div>
+    </div>
+  )
+}
