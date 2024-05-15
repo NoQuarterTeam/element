31d7b332021030c6cc9b9b1c77fabc6089545583
Jack Clackett <jack@noquarter.co>
2024-05-15 18:38:32 +0200

    add logs

--- packages/server/services/src/taskReminder.server.ts
+++ packages/server/services/src/taskReminder.server.ts
@@ -14,18 +14,25 @@ export async function createTaskReminder(task: Pick<Task, "id" | "date" | "start
     const hour = Number(task.startTime.split(":")[0])
     const minute = Number(task.startTime.split(":")[1])
     // upstashs servers on utc
+
     const reminderDateTime = dayjs(task.date)
       .set("hour", hour)
       .set("minute", minute)
       .subtract(reminderHash[task.reminder].hours, "hours")
       .subtract(reminderHash[task.reminder].minutes, "minutes")
-      .unix()
+
+    console.log({ date: task.date })
+    console.log({ hash: reminderHash[task.reminder] })
+
+    console.log({ hour, minute })
+
+    console.log(reminderDateTime.format())
 
     const headers = new Headers()
     headers.append("Content-Type", "application/json")
 
     const job = await qstash.publishJSON({
-      notBefore: reminderDateTime,
+      notBefore: reminderDateTime.unix(),
       body: JSON.stringify({ id: task.id } satisfies TaskReminderBody),
       headers,
       url: IS_DEV ? "https://element.requestcatcher.com" : `${FULL_WEB_URL}/api/task-reminder`,
